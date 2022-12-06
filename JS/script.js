d28b27b177a7d05af997b868438a41c5dc2c4602
Denio Nhanale <dnhanale@gmail.com>
2022-12-06 06:39:16 +0200

    2nd Commit

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,37 @@
+//Declare empty array
 let myLibrary = [];
 
-function Book() {
-
+//Object constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+   // this.info = function() {
+   //     return (title + author + ", " + pages + ", " + read)
+  //  }
 }
 
-function addBookToLibrary() {
-    
+//Function for addding book to the array
+function addBookToLibrary(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+// Function to display library array to cards
+function displayBooksOnPage() {
+    const books = document.querySelector(".books");
+
+    //Loop over the library array and display the cards
+    myLibrary.forEach(myLibrary => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        books.appendChild(card);
+        for (let key in myLibrary) {
+            console.log(`${key}: ${myLibrary[key]}`);
+            const para = document.createElement("p");
+            para.textContent = (`${key}: ${myLibrary[key]}`);
+            card.appendChild(para);
+        }
+    })
 }
